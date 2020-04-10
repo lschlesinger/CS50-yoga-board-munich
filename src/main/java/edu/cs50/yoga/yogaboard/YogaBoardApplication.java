8a3b96108c5e75b18d187eb8d69f6fe7ba9a0689
Lorena Schlesinger <lorena.schlesinger@online.de>
2020-04-10 21:51:48 +0200

    Update initial setup

---
+++ src/main/java/edu/cs50/yoga/yogaboard/YogaBoardApplication.java
@@ -0,0 +1,13 @@
+package edu.cs50.yoga.yogaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogaBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogaBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/cs50/yoga/yogaboard/YogaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cs50.yoga.yogaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogaBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
