281d87b4b122b4708a6df94f44e596be7931b000
Lorena Schlesinger <lorena.schlesinger@online.de>
2020-04-13 15:02:04 +0200

    add explanation to home page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {en_US, NgZorroAntdModule, NZ_I18N} from 'ng-zorro-antd';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {registerLocaleData} from '@angular/common';
 import en from '@angular/common/locales/en';
-import { HomeComponent } from './pages/home/home.component';
-import { DetailsComponent } from './pages/details/details.component';
-import { CreateComponent } from './pages/create/create.component';
+import {HomeComponent} from './pages/home/home.component';
+import {DetailsComponent} from './pages/details/details.component';
+import {CreateComponent} from './pages/create/create.component';
 
 registerLocaleData(en);
 
@@ -31,7 +31,8 @@ registerLocaleData(en);
     BrowserAnimationsModule,
     ReactiveFormsModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  providers: [{provide: NZ_I18N, useValue: en_US}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
